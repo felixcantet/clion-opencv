57bdf0d29a325e62996064cde5bd983f92e759ad
Sofiane <sofiane.benembareck@hotmail.com>
2021-04-20 19:03:45 +0200

    0.3s de generation avec opencl !

--- main.cpp
+++ main.cpp
@@ -28,12 +28,9 @@ const int POSE_PAIRS[20][2] =
 
 cv::Mat frame;
 
-cv::Rect handRoi;
-
 std::string protoFile = "../Hand/pose_deploy.prototxt";
 std::string weightsFile = "../Hand/pose_iter_102000.caffemodel";
 
-void CaptureHand(cv::dnn::Net& net);
 void DisplayVideo(const char*, cv::dnn::Net& net);
 
 int main()
@@ -43,6 +40,9 @@ int main()
 
     cv::dnn::Net net = cv::dnn::readNetFromCaffe(protoFile, weightsFile);
 
+    net.setPreferableTarget(cv::dnn::DNN_TARGET_OPENCL);
+    net.setPreferableBackend(cv::dnn::DNN_BACKEND_DEFAULT);
+
     //CaptureHand(net);
 
     DisplayVideo(nullptr, net);
@@ -50,60 +50,12 @@ int main()
     return 0;
 }
 
-void CaptureHand(cv::dnn::Net& net){
-    cv::VideoCapture cap;
-    cap.open(0);
-
-    cv::Mat frameTmp;
-
-    //si cap n’est pas ouvert, quitter la fonction
-    if (!cap.isOpened())
-        return;
-
-    cap.read(frameTmp);
-
-    int frameWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH);
-    int frameHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
-
-    float aspect_ratio = (float)frameWidth/(float)frameHeight;
-
-    int inHeight = 240;
-    int inWidth = int(aspect_ratio*inHeight);
-
-    cv::Rect roi(0,0,inWidth,inHeight);
-
-    while(true)
-    {
-        cv::putText(frameTmp, "Move your hand in the Rect and Press Enter", cv::Point(50, 50), cv::FONT_HERSHEY_COMPLEX, .8, cv::Scalar(255, 50, 0), 2);
-        cv::rectangle(frameTmp, roi.tl(), roi.br(), cv::Scalar(0, 255, 0), 1.f);
-        cv::imshow("Capture Hand", frameTmp);
-
-        int key = cv::waitKey(DELAY);
-        if(key == 13 || key == 27)
-        {
-            std::cout << "Key pressed = " << key << std::endl;
-
-            cv::Mat cropped = frameTmp(roi);
-            handRoi = roi;
-
-//            cv::Mat inpBlob = cv::dnn::blobFromImage(cropped, 1.0 / 255, cv::Size(inWidth, inHeight), cv::Scalar(0, 0, 0), false, false);
-//            net.setInput(inpBlob);
-//            cv::Mat output = net.forward();
-
-            break;
-        }
-
-        cap.read(frameTmp);
-    }
-
-    cap.release();
-    cv::destroyAllWindows();
-}
-
 void DisplayVideo(const char* videoname, cv::dnn::Net& net){
 
     cv::VideoCapture cap;
     int nPoints = 22;
+
+    int everyFrame = 0;
 
     //si videoname n’est pas null, ouvrir la DisplayVideo dans cap, sinon ouvrir la camera 0
     if (videoname != nullptr)
@@ -118,29 +70,35 @@ void DisplayVideo(const char* videoname, cv::dnn::Net& net){
     int frameWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH);
     int frameHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
     float aspect_ratio = (float)frameWidth/(float)frameHeight;
-    int inHeight = 200;
-    int inWidth = (int(aspect_ratio*inHeight) * 8) / 8;
+    int inHeight = 240;
+    int inWidth = int(aspect_ratio*inHeight);
+
+    float scaleFactor = 1.0f / 255.0f;
+    int nPairs = sizeof(POSE_PAIRS)/sizeof(POSE_PAIRS[0]);
+    std::vector<cv::Point> points(nPoints);
 
     //recuperer une image depuis cap et la stocker dans frame
     cap.read(frame);
-
+    cv::Mat output;
     //tant que nextinput n’est pas vide
     while(!frame.empty())
     {
         double t = (double) cv::getTickCount();
 
-        cv::Mat cropped = frame(handRoi);
-
-        cv::Mat inpBlob = cv::dnn::blobFromImage(frame, 1.0 / 255, cv::Size(inWidth, inHeight), cv::Scalar(0, 0, 0), false, false);
-        net.setInput(inpBlob);
-        cv::Mat output = net.forward();
+        if(everyFrame % 2 == 0) {
+            cv::Mat inpBlob = cv::dnn::blobFromImage(frame, scaleFactor, cv::Size(inWidth, inHeight), cv::Scalar(0, 0, 0), false, false);
+            net.setInput(inpBlob);
+            everyFrame = 0;
+            output = net.forward();
+        }
+        everyFrame++;
 
         int H = output.size[2];
         int W = output.size[3];
 
+
         // find the position of the body parts
-        std::vector<cv::Point> points(nPoints);
-        for (int n=0; n < nPoints; n++)
+        for (int n=0; n < nPoints; ++n)
         {
             // Probability map of corresponding body's part.
             cv::Mat probMap(H, W, CV_32F, output.ptr(0,n));
@@ -152,9 +110,7 @@ void DisplayVideo(const char* videoname, cv::dnn::Net& net){
             points[n] = maxLoc;
         }
 
-        int nPairs = sizeof(POSE_PAIRS)/sizeof(POSE_PAIRS[0]);
-
-        for (int n = 0; n < nPairs; n++)
+        for (int n = 0; n < nPairs; ++n)
         {
             // lookup 2 connected body/hand parts
             cv::Point2f partA = points[POSE_PAIRS[n][0]];
@@ -163,7 +119,7 @@ void DisplayVideo(const char* videoname, cv::dnn::Net& net){
             if (partA.x<=0 || partA.y<=0 || partB.x<=0 || partB.y<=0)
                 continue;
 
-            cv::line(frame, partA, partB,  cv::Scalar(0,255,255), 8);
+            cv::line(frame, partA, partB,  cv::Scalar(0,255,255), 4);
             cv::circle(frame, partA, 8,  cv::Scalar(0,0,255), -1);
             cv::circle(frame, partB, 8,  cv::Scalar(0,0,255), -1);
         }
